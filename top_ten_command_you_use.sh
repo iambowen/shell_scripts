485d689b8ce829f99f0b3a07c362a24d0bf84494
bwma <bwma@thoughtworks.com>
2013-01-28 13:37:51 +0800

    you need to read the history file then do the listing, for the script is running in a sub process which does not have history.

--- top_ten_command_you_use.sh
+++ top_ten_command_you_use.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-#use source top_ten_command_you_use.sh or . top_ten_command_you_use.sh
+history -r ~/.bash_history
 history|awk '{print $2}'|awk 'BEGIN {FS="|"} {print $1}'|sort|uniq -c|sort -rn|head -10
