d7eb71ec120c715d10117834d9f0cb9878c1e488
bowen <iambowen.m@gmail.com>
2013-04-25 15:58:33 +0800

    Update top_ten_command_you_use.sh
    just use the simple way to calculate the commands usage.

--- top_ten_command_you_use.sh
+++ top_ten_command_you_use.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 history -r ~/.bash_history
-history | cut -d " " -f2- | awk  'BEGIN {FS="|"}; {for(i = 1; i <= NF; i++) print $i;}' | awk '{print $1}'| sort | uniq -c|sort -rn|head -10
+history | cut -d " " -f2- | awk  '{print $2}' | awk '{print $1}'| sort | uniq -c | sort -rn | head -10
